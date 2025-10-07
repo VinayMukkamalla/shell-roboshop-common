82d07e7440346e8689a4576d0ed4f8a927c5650a
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-10-07 22:22:48 +0530

    adding files to common repo

--- common.sh
+++ common.sh
@@ -11,6 +11,7 @@ START_TIME=$(date +%s)
 mkdir -p $LOG_FOLDER
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 DIR_PATH=$PWD
+MONGODB_HOST=mongodb.vinaymukkamalla.fun
 
 echo " script started execution at : $(date)" | tee -a $LOG_FILE
 
@@ -19,6 +20,45 @@ check_root(){
         echo "ERROR:: you are not allowed to run this script use root privilege"
         exit 1
     fi
+}
+
+nodejs_setup(){
+    dnf module disable nodejs -y &>>$LOG_FILE
+    VALIDATE $? "disabling nodejs "
+    dnf module enable nodejs:20 -y &>>$LOG_FILE
+    VALIDATE $? "enabling nodejs version 20 "
+    dnf install nodejs -y &>>$LOG_FILE
+    VALIDATE $? "installing nodejs "
+    npm install &>>$LOG_FILE
+    VALIDATE $? "installing dependencies "
+}
+
+app_setup(){
+    mkdir -p /app &>>$LOG_FILE
+    VALIDATE $? "creating app directory "
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOG_FILE
+    VALIDATE $? "Downloading $app_name application"
+    cd /app &>>$LOG_FILE
+    VALIDATE $? "changing to /app Directory"
+    rm -rf /app/*  &>>$LOG_FILE
+    VALIDATE $? "removing existing $app_name application code"
+    unzip /tmp/$app_name.zip &>>$LOG_FILE
+    VALIDATE $? "unzipping $app_name application"
+}
+
+service_setup(){
+    cp $DIR_PATH/$app_name.service /etc/systemd/system/$app_name.service        
+    systemctl daemon-reload
+    systemctl enable $app_name &>>$LOG_FILE
+    VALIDATE $? "enlabling $app_name "
+    systemctl start $app_name &>>$LOG_FILE
+    VALIDATE $? "starting $app_name service "
+
+}
+
+app_restart(){
+    systemctl restart $app_name
+    VALIDATE $? "restarted $app_name service"
 }
 
 VALIDATE(){
